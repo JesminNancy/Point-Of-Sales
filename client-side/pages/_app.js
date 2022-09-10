a5860b2b6ed50e5fad373898d6e49ce42a15055b
JesminNancy <jesminnancy@hotmail.com>
2022-09-10 15:15:01 +0600

    Routing

--- client-side/pages/_app.js
+++ client-side/pages/_app.js
@@ -1,10 +1,25 @@
+import NextNProgress from "nextjs-progressbar";
 import '../styles/animate.min.css';
 import '../styles/bootstrap.min.css';
 import '../styles/fontawesome.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+        
+  );
+  
+
 }
 
-export default MyApp
+ export default MyApp
--- client-side/pages/category.js
+++ client-side/pages/category.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
+import NavMenuDesktop from '../components/NavMenuDesktop'
+
+export default class category extends Component {
+  render() {
+    return (
+      <div>
+        <NavMenuDesktop/>
+      </div>
+    )
+  }
+}
 
