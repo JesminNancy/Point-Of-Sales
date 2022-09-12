c7235326e01eb305bee29966fa12c138df0c20ed
JesminNancy <jesminnancy@hotmail.com>
2022-09-12 20:55:41 +0600

    Cart Model Controller Migration

---
+++ server-side/database/migrations/2022_09_12_144749_cart_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_list',function (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('invoice_no',200);
+            $table->string('invoice_date',200);
+            $table->string('product_name',200);
+            $table->string('product_qty',200);
+            $table->string('product_unit_price',200);
+            $table->string('product_total_price',200);
+            $table->string('seller_name',200);
+            $table->string('product_icon',200);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
