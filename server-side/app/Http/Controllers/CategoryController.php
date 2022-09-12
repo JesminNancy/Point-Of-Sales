4eb01892dbee71e998cee83998ec12fc815168b9
JesminNancy <jesminnancy@hotmail.com>
2022-09-12 11:12:05 +0600

    Category List API

--- server-side/app/Http/Controllers/CategoryController.php
+++ server-side/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CategoryModel;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
-    //
+    function addCategory(Request $request){
+        $name=$request->input('name');
+        $imagepath=$request->file('image')->store('public/img');
+        $code=time();
+        $result=CategoryModel::insert(['cat_name'=>$name, 'cat_code'=>$code, 'cat_icon'=>$imagepath]);
+        return $result;
+    }
+
+    function selectCategory(){
+        $result=CategoryModel::all();
+        return $result;
+    }
+
+    function deleteCategory(Request $request){
+        $id= $request->id;
+        $cat_icon= CategoryModel::Where('id',$id)->get(['cat_icon']);
+        Storage::delete($cat_icon[0]['cat_icon']);
+        $result=CategoryModel::Where('id', $id)->delete();
+        return  $result;
+    }
+
+    function updateCategoryWithoutImage(Request $request){
+        $id=$request->input('id');
+        $name=$request->input('name');
+        $result=CategoryModel::Where('id', $id)->update(['cat_name'=>$name]);
+        return  $result;
+    }
+
+    function updateCategoryWithImage(Request $request){
+        $id= $request->input('id');
+
+        // Old Image Delete
+        $cat_icon= CategoryModel::Where('id',$id)->get(['cat_icon']);
+        Storage::delete($cat_icon[0]['cat_icon']);
+
+        // New Image Upload
+        $imagePath= $request->file('image')->store('public/img');
+
+
+        $name= $request->input('name');
+
+        $result= CategoryModel::Where('id', $id)->update(['cat_name'=>$name,'cat_icon'=>$imagePath]);
+
+        return $result;
+    }
 }
--- server-side/routes/api.php
+++ server-side/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,11 @@ Route::post('/addUser',[UserController::class,'AddUser']);
 Route::get('/selectUser',[UserController::class,'selectUser']);
 Route::get('/DeleteUser/{id}',[UserController::class,'DeleteUser']);
 Route::post('/UpdateUser',[UserController::class,'UpdateUser']);
+
+
+//Category......
+Route::post('/addCategory',[CategoryController::class,'addCategory']);
+Route::get('/selectCategory',[CategoryController::class,'selectCategory']);
+Route::delete('/deleteCategory/{id}',[CategoryController::class,'deleteCategory']);
+Route::post('/withoutiamge',[CategoryController::class,'updateCategoryWithoutImage']);
+Route::post('/withiamge',[CategoryController::class,'updateCategoryWithImage']);
